57147e14612dcc0916c3fcd56f52b2b7ad995a6d
NAKAGAWA Masaki <masaki.nakagawa@gmail.com>
2011-12-06 22:31:46 +0900

    initial commit

---
+++ lib/Test/Mock/LWP/Conditional.pm
@@ -0,0 +1,36 @@
+package Test::Mock::LWP::Conditional;
+
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+1;
+
+=head1 NAME
+
+Test::Mock::LWP::Conditional - A module that ...
+
+=head1 SYNOPSIS
+
+    use Test::Mock::LWP::Conditional;
+
+=head1 DESCRIPTION
+
+Test::Mock::LWP::Conditional is
+
+=head1 METHODS
+
+=head1 AUTHOR
+
+NAKAGAWA Masaki E<lt>masaki@cpan.orgE<gt>
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
