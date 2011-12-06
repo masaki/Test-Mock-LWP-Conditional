26841dec1bebffd732d3e00bd91a7d48f82cea8b
NAKAGAWA Masaki <masaki.nakagawa@gmail.com>
2011-12-06 22:49:21 +0900

    impl and compile ok

--- lib/Test/Mock/LWP/Conditional.pm
+++ lib/Test/Mock/LWP/Conditional.pm
@@ -3,8 +3,62 @@ package Test::Mock::LWP::Conditional;
 use 5.008001;
 use strict;
 use warnings;
+use LWP::UserAgent;
+use Scalar::Util qw(blessed refaddr);
+use Sub::Install qw(install_sub);
+use Class::Method::Modifiers qw(install_modifier);
+use Test::Mock::LWP::Conditional::Container;
 
 our $VERSION = '0.01';
+$VERSION = eval $VERSION;
+
+our $Stubs = +{ __GLOBAL__ => +{} };
+
+sub _set_stub {
+    my ($key, $uri, $res) = @_;
+
+    $Stubs->{$key} ||= +{};
+    $Stubs->{$key}->{$uri} ||= Test::Mock::LWP::Conditional::Container->new;
+
+    $Stubs->{$key}->{$uri}->add($res);
+}
+
+sub _get_stub {
+    my ($key, $uri) = @_;
+
+    if (exists $Stubs->{$key} && exists $Stubs->{$key}->{$uri}) {
+        return $Stubs->{$key}->{$uri};
+    }
+    elsif (exists $Stubs->{__GLOBAL__}->{$uri}) {
+        return $Stubs->{__GLOBAL__}->{$uri};
+    }
+}
+
+sub stub_request {
+    my ($self, $uri, $res) = @_;
+    my $key = blessed($self) ? refaddr($self) : '__GLOBAL__';
+    _set_stub($key, $uri, $res);
+}
+
+sub reset_all {
+    $Stubs = +{ __GLOBAL__ => +{} };
+}
+
+{ # LWP::UserAgent injection
+    install_modifier('LWP::UserAgent', 'around', 'simple_request', sub {
+        my $orig = shift;
+        my ($self, $req, @rest) = @_;
+
+        my $stub = _get_stub(refaddr($self), $req->uri);
+        return $stub ? $stub->execute($req) : $orig->(@_);
+    });
+
+    install_sub({
+        code => __PACKAGE__->can('stub_request'),
+        into => 'LWP::UserAgent',
+        as   => 'stub_request',
+    });
+}
 
 1;
 
@@ -18,9 +72,19 @@ Test::Mock::LWP::Conditional - A module that ...
 
 =head1 DESCRIPTION
 
-Test::Mock::LWP::Conditional is
+This module stubs out LWP::UserAgent's request.
 
 =head1 METHODS
+
+=over 4
+
+=item * stub_request($uri, $res)
+
+=item * reset_all
+
+Clear all stub requests.
+
+=back
 
 =head1 AUTHOR
 
@@ -32,5 +96,9 @@ This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
 
 =head1 SEE ALSO
+
+L<Test::Mock::LWP>, L<Test::Mock::LWP::Dispatch>, L<Test::MockHTTP>, L<Test::LWP::MockSocket::http>
+
+L<LWP::UserAgent>
 
 =cut
